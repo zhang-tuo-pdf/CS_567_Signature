fe3c424c279255dbd67eaf8866631f9dee9d7f9b
shiqij101 <57372195+shiqij101@users.noreply.github.com>
2024-05-01 09:45:53 -0700

    updated version of SNN model
    with encoder options = 'cnn' or 'resnet'

--- model/snn.py
+++ model/snn.py
@@ -1,54 +1,88 @@
-# Siamese Network model Implementation with Contrastive Loss
+# Siamese Network model Implementation + Contrastive Loss
+# model encoder choices from ['cnn', 'resnet'], Default='cnn'
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+
 
 class SiameseNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, model='cnn'):
+        self.model = model
         super().__init__()
-        self.cnn1 = nn.Sequential(
 
-            nn.Conv2d(1, 96, kernel_size=11, stride=1),
-            nn.BatchNorm2d(96),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
+        # Original SigNet CNN encoder
+        if self.model == 'cnn':
+            self.cnn1 = nn.Sequential(
 
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
-            nn.Dropout2d(p=0.3),
+                nn.Conv2d(1, 96, kernel_size=11, stride=1),
+                nn.BatchNorm2d(96),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(3, stride=2),
 
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(384),
-            nn.ReLU(inplace=True),
+                nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(3, stride=2),
+                nn.Dropout2d(p=0.3),
 
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
-            nn.Dropout2d(p=0.3)
-        )
+                nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(384),
+                nn.ReLU(inplace=True),
 
-        # Assuming input size = 105x105x1, subject to change
-        self.fc1 = nn.Sequential(
-            nn.Linear(30976, 1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(p=0.5),
+                nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(3, stride=2),
+                nn.Dropout2d(p=0.3)
+            )
+            # Assuming input size = 105x105x1, subject to change
+            self.fc1 = nn.Sequential(
+                nn.Linear(30976, 1024),
+                nn.ReLU(inplace=True),
+                nn.Dropout2d(p=0.5),
 
-            nn.Linear(1024, 128),
-            nn.ReLU(inplace=True),
+                nn.Linear(1024, 128),
+                nn.ReLU(inplace=True),
 
-            # nn.Linear(128, 64),
-            # nn.ReLU(inplace=True),
+                nn.Linear(128, 2))
 
-            nn.Linear(128, 2))
+        # Resnet18 encoder with pretrained weights
+        elif self.model == 'resnet':
+            # Use ResNet18 pretrained weights except for newly-designed fc layers
+            self.resnet18 = models.resnet18(weights="DEFAULT")
+
+            original_first_layer = self.resnet18.conv1
+
+            # Create a new conv layer with in_channels=1
+            self.resnet18.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+            # Initialize the new conv layer's weights by averaging the pretrained weights across the color channels
+            with torch.no_grad():
+                self.resnet18.conv1.weight[:] = original_first_layer.weight.mean(dim=1, keepdim=True)
+
+            for param in self.resnet18.parameters():
+                param.requires_grad = False
+
+            self.resnet18.fc = nn.Sequential(
+                nn.Linear(self.resnet18.fc.in_features, 256),
+                nn.ReLU(inplace=True),
+
+                nn.Linear(256, 64),
+                nn.ReLU(inplace=True),
+
+                nn.Linear(64, 2)
+            )
 
     def forward_once(self, x):
-        output = self.cnn1(x)
-        output = output.view(output.size()[0], -1)
-        output = self.fc1(output)
+        if self.model == 'cnn':
+            output = self.cnn1(x)
+            output = output.view(output.size()[0], -1)
+            output = self.fc1(output)
+
+        elif self.model == 'resnet':
+            output = self.resnet18(x)
         return output
 
     def forward(self, input1, input2):
@@ -61,8 +95,8 @@ class SiameseNetwork(nn.Module):
 
 class ContrastiveLoss(nn.Module):
     '''
-    Loss = 1/2[ (Y)*EuDist^2 + (1-Y)*max(0, margin-EuDist)^2 ]
-        For Y=0 (dissimilar pair), pair with EuDist > margin will not contribute to loss
+    Loss = 1/2[ (1-Y)*EuDist^2 + (Y)*max(0, margin-EuDist)^2 ]
+        For Y=1 (dissimilar pair), pair with EuDist > margin will not contribute to loss
     '''
 
     def __init__(self, margin=2.0):
